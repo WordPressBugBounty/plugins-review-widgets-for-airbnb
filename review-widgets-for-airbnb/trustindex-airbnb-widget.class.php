ad2da789b96ec46add66647a137482c435a99847
Sean Murphy <sean@iamseanmurphy.com>
2024-10-29 14:30:23 +0000

    12.4

--- review-widgets-for-airbnb/trustindex-airbnb-widget.class.php
+++ review-widgets-for-airbnb/trustindex-airbnb-widget.class.php
@@ -38,17 +38,14 @@ break;
 }
 }
 if (!$wasError && $instance['ti-widget-ID']) {
-echo $trustindex_pm_airbnb->get_trustindex_widget($instance['ti-widget-ID']);
-}
-else if ($trustindex_pm_airbnb->is_noreg_linked()) {
-echo $trustindex_pm_airbnb->get_noreg_list_reviews();
-}
-else {
-echo $trustindex_pm_airbnb->error_box_for_admins(sprintf(__("Please fill out <strong>all the required fields</strong> in the <a href='%s'>widget settings</a> page", 'trustindex-plugin'), admin_url('admin.php?page='.$trustindex_pm_airbnb->get_plugin_slug().'/settings.php')));
+echo $trustindex_pm_airbnb->renderWidgetFrontend($instance['ti-widget-ID']);
+} else if ($trustindex_pm_airbnb->is_noreg_linked()) {
+echo $trustindex_pm_airbnb->renderWidgetFrontend();
+} else {
+echo $trustindex_pm_airbnb->frontEndErrorForAdmins(sprintf(__("Please fill out <strong>all the required fields</strong> in the <a href='%s'>widget settings</a> page", 'trustindex-plugin'), admin_url('admin.php?page='.$trustindex_pm_airbnb->get_plugin_slug().'/settings.php')));
 }
 echo $after_widget;
-}
-else {
+} else {
 }
 }
 function form($instance)
